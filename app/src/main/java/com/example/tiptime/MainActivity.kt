fc37cc26a0c3506ca97ea36c92d099741124a6d1
Yuandika Alfahreiza <yuandika11@gmail.com>
2021-11-14 19:39:32 +0700

    Adding some Comments

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -5,31 +5,57 @@ import android.os.Bundle
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
+/**
+ * Activity that displays a tip calculator.
+ */
 class MainActivity : AppCompatActivity() {
 
+    // Binding object instance with access to the views in the activity_main.xml layout
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Inflate the layout XML file and return a binding object instance
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Set the content view of the Activity to be the root view of the layout
         setContentView(binding.root)
+
+        // Set up a key listener on the EditText field to listen for "enter" button presses
         binding.calculateButton.setOnClickListener{ calculateTip()}
     }
 
+    /**
+     * Calculates the tip based on the user input.
+     */
     private fun calculateTip() {
+        // Get the decimal value from the cost of service text field
         val stringInTextField =binding.costOfService.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
+
+        // If the cost is null or 0, then display 0 tip and exit this function early.
         if (cost == null) {
             binding.tipResult.text = ""
             return
         }
+
+        // Get the tip percentage based on which radio button is selected
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
+
+        // Calculate the tip
         var tip = tipPercentage * cost
+
+        // If the switch for rounding up the tip toggled on (isChecked is true), then round up the
+        // tip. Otherwise do not change the tip value.
         if (binding.roundUpSwitch.isChecked) {
+
+            // Take the ceiling of the current tip, which rounds up to the next integer, and store
+            // the new value in the tip variable.
             tip = kotlin.math.ceil(tip)
         }
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
